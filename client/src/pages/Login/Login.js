eb47fe0569d47bcb1d46aa8496b0a11a101aa964
William Budoyo <williambu07@gmail.com>
2018-10-20 08:39:52 -0400

    reactstrap on login page

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react'
-import { Redirect } from 'react-router-dom'
-import API from "../../utils/API"
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { Button, Form, FormGroup, Label, Input, Container, Row, Col } from 'reactstrap';
+import API from "../../utils/API";
+
 
 class LoginForm extends Component {
     constructor() {
@@ -48,48 +50,45 @@ class LoginForm extends Component {
             return <Redirect to={{ pathname: this.state.redirectTo }} />
         } else {
             return (
-                <div>
+                <Container>
+                    <Row>
+                    <Col className="login-col" sm="12" md={{ size: 6, offset: 3 }} >
                     <h4>Login</h4>
-                    <form className="form-horizontal">
-                        <div className="form-group">
-                            <div className="col-1 col-ml-auto">
-                                <label className="form-label" htmlFor="username">Username</label>
-                            </div>
-                            <div className="col-3 col-mr-auto">
-                                <input className="form-input"
-                                    type="text"
-                                    id="username"
-                                    name="username"
-                                    placeholder="Username"
-                                    value={this.state.username}
-                                    onChange={this.handleChange}
-                                />
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <div className="col-1 col-ml-auto">
-                                <label className="form-label" htmlFor="password">Password: </label>
-                            </div>
-                            <div className="col-3 col-mr-auto">
-                                <input className="form-input"
-                                    placeholder="password"
-                                    type="password"
-                                    name="password"
-                                    value={this.state.password}
-                                    onChange={this.handleChange}
-                                />
-                            </div>
-                        </div>
-                        <div className="form-group ">
-                            <div className="col-7"></div>
-                            <button
-                                className="btn btn-primary col-1 col-mr-auto"
-                               
-                                onClick={this.handleSubmit}
-                                type="submit">Login</button>
-                        </div>
-                    </form>
-                </div>
+                    <Form>
+                        <FormGroup>
+                            <Label for="username"> Username</Label>
+                            <Input
+                                type="text"
+                                id="username"
+                                name="username"
+                                placeholder="Username"
+                                value={this.state.username}
+                                onChange={this.handleChange}     
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="password"> Password</Label>
+                            <Input
+                                placeholder="password"
+                                type="password"
+                                name="password"
+                                value={this.state.password}
+                                onChange={this.handleChange}     
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Button
+                            type="submit"
+                            color="danger"
+                            onClick = {this.handleSubmit}
+                            >
+                            Login
+                            </Button>
+                        </FormGroup>
+                    </Form>
+                    </Col>
+                    </Row>
+                </Container>
             )
         }
     }
