74908c4ae2055bcd684555235854ccb60e08753e
Roy <rxbrxy@gmail.com>
2018-10-20 11:36:00 -0400

    not yet functional

--- client/src/pages/AnswerSurvey/AnswerSurvey.js
+++ client/src/pages/AnswerSurvey/AnswerSurvey.js
@@ -1,12 +1,52 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../../components/Grid";
+import Jumbotron from "../../components/Jumbotron";
+import API from "../../utils/API";
 
 class AnswerSurvey extends Component {
 
-render() {
-    return(
-        <h1>answer survey</h1>
-    )
-}
-};
-
+    state = {
+        item: {}
+      };
+      // When this component mounts, grab the item with the _id of this.props.match.params.id
+      // e.g. localhost:3000/items/599dcb67f0f16317844583fc
+      componentDidMount() {
+        API.getItem(this.props.match.params.id)
+          .then(res => this.setState({ item: res.data }))
+          .catch(err => console.log(err));
+      }
+    
+      render() {
+        return (
+          <Container fluid>
+            <Row>
+              <Col size="md-12">
+                <Jumbotron>
+                  <h1>
+                    {this.state.item.title} by {this.state.item.author}
+                  </h1>
+                </Jumbotron>
+              </Col>
+            </Row>
+            <Row>
+              <Col size="md-10 md-offset-1">
+                <article>
+                  <h1>Synopsis</h1>
+                  <p>
+                    {this.state.item.synopsis}
+                  </p>
+                </article>
+              </Col>
+            </Row>
+            <Row>
+              <Col size="md-2">
+                <Link to="/">← Back to Authors</Link>
+              </Col>
+            </Row>
+          </Container>
+        );
+      }
+    }
+    
 export default AnswerSurvey;
--- client/src/pages/CreateSurvey/CreateSurvey.js
+++ client/src/pages/CreateSurvey/CreateSurvey.js
@@ -1,12 +1,125 @@
 import React, { Component } from "react";
+import DeleteBtn from "../../components/DeleteBtn";
+import Jumbotron from "../../components/Jumbotron";
+import API from "../../utils/API";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../../components/Grid";
+import { List, ListItem } from "../../components/List";
+import { Input, TextArea, FormBtn } from "../../components/Form";
 
 class CreateSurvey extends Component {
 
-render() {
-    return(
-        <h1>create survey</h1>
-    )
-}
-};
+    state = {
+        items: [],
+        title: "",
+        // author: "",
+        synopsis: ""
+      };
+    
+      componentDidMount() {
+        this.loadItems();
+      }
+    
+      loadItems = () => {
+        API.getItems()
+          .then(res =>
+            this.setState({ items: res.data, title: "", synopsis: "" })
+          )
+          .catch(err => console.log(err));
+      };
+    
+      deleteItem = id => {
+        API.deleteItem(id)
+          .then(res => this.loadItems())
+          .catch(err => console.log(err));
+      };
+    
+      handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+          [name]: value
+        });
+      };
+    
+      handleFormSubmit = event => {
+        event.preventDefault();
+        if (this.state.title
+          // && this.state.author
+          ) {
+          API.saveItem({
+            title: this.state.title,
+            // author: this.state.author,
+            synopsis: this.state.synopsis
+          })
+            .then(res => this.loadItems())
+            .catch(err => console.log(err));
+        }
+      };
+    
+      render() {
+        return (
+          <Container fluid>
+            <Row>
+              <Col size="md-6">
+                <Jumbotron>
+                  <h1>Make a ballot</h1>
+                </Jumbotron>
+                <form>
+                  <Input
+                    value={this.state.title}
+                    onChange={this.handleInputChange}
+                    name="title"
+                    placeholder="Title (required)"
+                  />
+                  {/* <Input
+                    value={this.state.author}
+                    onChange={this.handleInputChange}
+                    name="author"
+                    placeholder="Author (required)"
+                  /> */}
+                  <TextArea
+                    value={this.state.synopsis}
+                    onChange={this.handleInputChange}
+                    name="details"
+                    placeholder="Details (Optional)"
+                  />
+                  <FormBtn
+                    disabled={!(
+                      // this.state.author && 
+                      this.state.title)}
+                    onClick={this.handleFormSubmit}
+                  >
+                    Submit
+                  </FormBtn>
+                </form>
+              </Col>
+              <Col size="md-6 sm-12">
+                <Jumbotron>
+                  <h1>Current Options</h1>
+                </Jumbotron>
+                {this.state.items.length ? (
+                  <List>
+                    {this.state.items.map(item => (
+                      <ListItem key={item._id}>
+                        <Link to={"/items/" + item._id}>
+                          <strong>
+                            {item.title} 
+                            {/* by {item.author */}
+                            }
+                          </strong>
+                        </Link>
+                        <DeleteBtn onClick={() => this.deleteItem(item._id)} />
+                      </ListItem>
+                    ))}
+                  </List>
+                ) : (
+                  <h3>Current Options</h3>
+                )}
+              </Col>
+            </Row>
+          </Container>
+        );
+      }
+    }
 
 export default CreateSurvey;
