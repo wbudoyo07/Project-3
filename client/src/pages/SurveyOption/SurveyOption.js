b3afe5fe68a7249da18f3f1077dd61c0a486e5f5
William Budoyo <williambu07@gmail.com>
2018-10-22 20:20:07 -0400

    added button that link to the route

--- client/src/pages/SurveyOption/SurveyOption.js
+++ client/src/pages/SurveyOption/SurveyOption.js
@@ -1,52 +1,39 @@
 import React, { Component } from 'react';
 import Nav from "../../components/Navbar";
-import API from "../../utils/API";
+import { Container, Row, Col, Button } from 'reactstrap';
 
 class SurveyOption extends Component {
-  constructor() {
-    super()
-    this.state = {
-      loggedIn: false,
-      username: null
-    }
-
-    this.getUser = this.getUser.bind(this)
-    this.componentDidMount = this.componentDidMount.bind(this)
-    this.updateUser = this.updateUser.bind(this)
-  }
-
-  componentDidMount() {
-    this.getUser()
-  }
-
-  updateUser (userObject) {
-    this.setState(userObject)
-  }
-
-  getUser() {
-    API.loginData().then(response => {
-      console.log('Get user response: ')
-      console.log(response.data)
-      if (response.data.userLoggedin) {
-        console.log('Get User: There is a user saved in the server session: ')
-
-        this.setState({
-          loggedIn: true,
-          username: response.data.userLoggedin.username
-        })
-      } else {
-        console.log('Get user: no user');
-        this.setState({
-          loggedIn: false,
-          username: null
-        })
-      }
-    })
-  }
 
   render() {
     return (
-      <Nav />
+      <Container fluid>
+        <Nav />
+        <Row>
+          <Col>
+            <Button
+              href="/createsurvey"
+              outline color ="info"
+              size="lg"
+              block
+            >
+              Create Survey
+            </Button>
+          </Col>
+          </Row>
+          <Row>
+          <Col>
+            <Button
+            type ="submit"
+            href="/previoussurvey"
+            outline color ="info"
+            size ="lg"
+            block
+            >
+              Previous Survey
+            </Button>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
