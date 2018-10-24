ddd76fc64ce45100f5dd35fc612eaa0791f4e802
Roy <rxbrxy@gmail.com>
2018-10-23 22:29:15 -0400

    The purge has begun

--- client/src/components/List/SortableItem.js
+++
@@ -1,8 +0,0 @@
-
-import React, {Component} from 'react';
-import {render} from 'react-dom';
-import {SortableContainer, SortableElement, arrayMove} from 'react-sortable-hoc';
-
-const SortableItem = SortableElement(({value}) => <li>{value}</li>);
-
-export default SortableItem
