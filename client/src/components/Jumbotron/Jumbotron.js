0045bd7a84ac99366271bdaab4e4a3f20c1d1d13
Roy <rxbrxy@gmail.com>
2018-10-25 14:38:47 -0400

    skillsaw

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Jumbotron = ({ children }) => (
   <div
-    style={{ height: 100, clear: "both", paddingTop: 30, textAlign: "center" }}
+    style={{ height: 200, clear: "both", paddingTop: 30, textAlign: "center", backgroundColor: "#007BA7" }}
     className="jumbotron"
   >
     {children}
